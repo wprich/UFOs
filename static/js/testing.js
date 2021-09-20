2271822b57e5a86c996d447fdb0ed411f5305065
William <wprich704@gmail.com>
2021-09-19 19:06:08 -0700

    Added test code and data building tools

--- static/js/testing.js
+++ static/js/testing.js
@@ -19,3 +19,23 @@ function doubleAddition(c, d) {
   }
 //Converted to a fat arrow format of the above function
 doubleAddition = (c, d) => addition(c, d) * 2;
+
+//Declare a list
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+//For loop in javascript
+function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+
+//Declare a list
+let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+//For loop to print to the console
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+//Another for loop
+for (var i = 0; i < 5; i++) {
+    console.log("I am " + i);
+ }
